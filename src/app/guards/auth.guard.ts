2182973723458143c9c3c9c44bcc98233b1e3b86
Álex Maciel <aweasley.gina@gmail.com>
2025-03-12 11:47:42 -0300

    criando metodo que verifica se o usuario esta autenticado

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,18 +1,30 @@
 import { Injectable } from '@angular/core';
-import { CanLoad, Route, UrlSegment, UrlTree } from '@angular/router';
+import { CanLoad, Route, UrlSegment, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanLoad {
+  constructor(private router: Router) {
+
+  }
   canLoad(
     route: Route,
     segments: UrlSegment[]): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return new Promise((res) => {
-      setTimeout(() => {
-        res(true);
-      }, 5000);
-    });
+
+      const token = localStorage.getItem('token');
+
+      if (!token) {
+        this.notAuthenticated();
+      }
+
+      return true;
+  }
+
+  notAuthenticated() {
+    localStorage.clear();
+    this.router.navigate(['/login']);
+    return false;
   }
 }
