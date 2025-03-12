3d43135c4c05750ed51c96c7e1c7b4f4d3f21c66
Álex Maciel <aweasley.gina@gmail.com>
2025-03-11 21:46:32 -0300

    criando metodo para buscar o access token

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,19 +1,41 @@
 import { Injectable } from '@angular/core';
 import { SpotifyConfiguration } from 'src/environments/environment';
+import Spotify  from 'spotify-web-api-js';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SpotifyService {
 
-  constructor() { }
+  spotifyApi: Spotify.SpotifyWebApiJs = null;
 
-  obterUrlLogin() {
+  constructor() {
+    this.spotifyApi = new Spotify();
+   }
+
+  getUrlLogin() {
     const authEndpoint = `${SpotifyConfiguration.authEndpoint}?`;
     const clientId = `client_id=${SpotifyConfiguration.clientId}&`;
     const redirectUrl = `redirect_uri=${SpotifyConfiguration.redirectUrl}&`;
     const scopes = `scope=${SpotifyConfiguration.scopes.join('%20')}&`;
     const responseType = `response_type=token&show_dialog=true`;
     return authEndpoint + clientId + redirectUrl + scopes + responseType; 
+  }
+
+  getTokenUrlCallback() {
+    console.log(window.location.hash)
+    if (!window.location.hash)
+      return '';
+
+    const params = window.location.hash.substring(1).split('&');
+    console.log(params)
+    return params[0].split('=')[1];
+
+    return '';
+  }
+
+  defineAccessToken(token: string) {
+    this.spotifyApi.setAccessToken(token);
+    localStorage.setItem('token', token);
   }
 }
