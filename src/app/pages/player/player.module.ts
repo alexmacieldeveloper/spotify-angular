0f9882da38477dd2ec1110860857f63e7e1efb65
Álex Maciel <aweasley.gina@gmail.com>
2025-03-18 14:33:58 -0300

    chamando componente button-menu no player module

--- src/app/pages/player/player.module.ts
+++ src/app/pages/player/player.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ButtonMenuComponent } from 'src/app/components/button-menu/button-menu.component';
 import { LeftSideMenuComponent } from 'src/app/components/left-side-menu/left-side-menu.component';
 import { PlayerComponent } from './player.component';
 import { PlayerRotas } from './player.routes';
@@ -8,7 +9,8 @@ import { PlayerRotas } from './player.routes';
 @NgModule({
     declarations: [
     PlayerComponent,
-    LeftSideMenuComponent
+    LeftSideMenuComponent,
+    ButtonMenuComponent
   ],
     imports: [
         CommonModule,
