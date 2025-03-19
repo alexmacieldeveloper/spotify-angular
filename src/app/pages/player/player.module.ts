9ddc3cec7c1891d11a26588f5ae7ecd4ad3003b0
Álex Maciel <aweasley.gina@gmail.com>
2025-03-18 22:41:59 -0300

    declarando component FooterUser

--- src/app/pages/player/player.module.ts
+++ src/app/pages/player/player.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { ButtonMenuComponent } from 'src/app/components/button-menu/button-menu.component';
+import { FooterUserComponent } from 'src/app/components/footer-user/footer-user.component';
 import { LeftSideMenuComponent } from 'src/app/components/left-side-menu/left-side-menu.component';
 import { PlayerComponent } from './player.component';
 import { PlayerRotas } from './player.routes';
@@ -11,7 +12,8 @@ import { PlayerRotas } from './player.routes';
     declarations: [
     PlayerComponent,
     LeftSideMenuComponent,
-    ButtonMenuComponent
+    ButtonMenuComponent,
+    FooterUserComponent
   ],
     imports: [
         CommonModule,
