12f5698d6212ab7ff2bde85fc1028b02610e41d8
Álex Maciel <aweasley.gina@gmail.com>
2025-03-18 22:42:47 -0300

    criando component que fica no rodape do menu lateral

---
+++ src/app/components/footer-user/footer-user.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
+import { IUser } from 'src/app/interfaces/IUser';
+import { SpotifyService } from 'src/app/services/spotify.service';
+
+@Component({
+  selector: 'app-footer-user',
+  templateUrl: './footer-user.component.html',
+  styleUrls: ['./footer-user.component.scss']
+})
+export class FooterUserComponent implements OnInit {
+
+  exitIcon = faSignOutAlt;
+  user: IUser = null;
+
+  constructor(private spotifyService: SpotifyService) { }
+
+  ngOnInit(): void {
+    this.user = this.spotifyService.user;
+  }
+
+  logout() {
+    this.spotifyService.logout();
+  }
+}
