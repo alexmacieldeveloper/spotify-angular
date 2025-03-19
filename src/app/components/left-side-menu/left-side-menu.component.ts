37971ac07e63b9e197172e6c4f4a696c41ffd43a
Álex Maciel <aweasley.gina@gmail.com>
2025-03-18 21:30:29 -0300

    exibindo as playlists do usuario

--- src/app/components/left-side-menu/left-side-menu.component.ts
+++ src/app/components/left-side-menu/left-side-menu.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { faGuitar, faHome, faMusic, faSearch } from '@fortawesome/free-solid-svg-icons';
+import { SpotifyService } from 'src/app/services/spotify.service';
+import { IPlaylist } from './../../interfaces/IPlaylist';
 
 @Component({
   selector: 'app-left-side-menu',
@@ -9,17 +11,24 @@ import { faGuitar, faHome, faMusic, faSearch } from '@fortawesome/free-solid-svg
 export class LeftSideMenuComponent implements OnInit {
   menuSelected = 'Home';
 
+  playlists: IPlaylist[] = [];
+
   homeIcon = faHome;
   searchIcon = faSearch;
   artistsIcon = faGuitar;
   playlistIcon = faMusic;
   
-  constructor() { }
+  constructor(private spotifyService: SpotifyService ) { }
 
   ngOnInit(): void {
+    this.searchPlaylits();
   }
 
   buttonClick(button: string) {
     this.menuSelected = button;
+  }
+
+  async searchPlaylits() {
+    this.playlists = await this.spotifyService.searchPlaylistUser();
   }
 }
