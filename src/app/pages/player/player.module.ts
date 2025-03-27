bef8d55872228bf07e30582b228ab33418e11fc5
Álex Maciel <aweasley.gina@gmail.com>
2025-03-26 22:59:57 -0300

    importando component que exibe os 5 artistas mais escutados

--- src/app/pages/player/player.module.ts
+++ src/app/pages/player/player.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ArtistItemImageComponent } from 'src/app/components/artist-item-image/artist-item-image.component';
 import { ButtonMenuComponent } from 'src/app/components/button-menu/button-menu.component';
 import { FooterUserComponent } from 'src/app/components/footer-user/footer-user.component';
 import { LeftSideMenuComponent } from 'src/app/components/left-side-menu/left-side-menu.component';
 import { RecentSearchesComponent } from 'src/app/components/recent-searches/recent-searches.component';
 import { RightPanelComponent } from 'src/app/components/right-panel/right-panel.component';
 import { TopArtistComponent } from 'src/app/components/top-artist/top-artist.component';
+import { TopArtistsComponent } from 'src/app/components/top-artists/top-artists.component';
 import { HomeComponent } from '../home/home.component';
 import { PlayerComponent } from './player.component';
 import { PlayerRotas } from './player.routes';
@@ -22,7 +24,9 @@ import { PlayerRotas } from './player.routes';
     HomeComponent,
     TopArtistComponent,
     RightPanelComponent,
-    RecentSearchesComponent
+    RecentSearchesComponent, 
+    TopArtistsComponent,
+    ArtistItemImageComponent
   ],
     imports: [
         CommonModule,
